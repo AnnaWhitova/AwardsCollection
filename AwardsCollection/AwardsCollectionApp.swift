94f8031cc17a13d0edcb3761e21e56c72c76d117
AnnaWhitova <159627315+AnnaWhitova@users.noreply.github.com>
2024-09-18 18:54:25 +0300

    Initial Commit

---
+++ AwardsCollection/AwardsCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionApp.swift
+//  AwardsCollection
+//
+//  Created by Анна Белова on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
